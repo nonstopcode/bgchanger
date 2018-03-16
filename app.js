8e5511c409e4205ba7f80e5e2b4a7ba4658b0108
Drew <drew@Drews-MacBook-Air.local>
2018-03-16 12:30:21 -0700

    new

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ var app = express();
 var server= require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
+
+var path = require('path');
+
+
+app.use(express.static('public'));
+
+users = [];
+connections = [];
+
 server.listen(process.env.PORT || 3000);
+console.log('Server Running');
 
-
-//redirect / to our index.html file
-app.get('/', function(req, res,next) {  
+app.get('/', function(req, res){
     res.sendFile(__dirname + '/client/index.html');
 });
 
